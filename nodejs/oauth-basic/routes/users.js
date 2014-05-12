c21078b8860003e28da9a5d0d7051c7e4084aef3
이 승철 <tadoli2007@gmail.com>
2014-05-12 09:39:28 +0900

    동작하는 코드로 완성

--- nodejs/oauth-basic/routes/users.js
+++ nodejs/oauth-basic/routes/users.js
@@ -11,17 +11,25 @@ router.get('/', function(req, res) {
   if(!req.session.accessToken) {
     // session 에 accessToken이 없으면 authorize 시도
     // 또는 DB에서 해당 사용자의 access token 정보를 가져와서 시도(그러나 이번 실습에선 복잡해지므로 하지 않음)
+    res.redirect("https://apis.daum.net/oauth2/authorize?response_type=code&client_id=1234567890&redirect_uri=http://172.16.99.99:3000/oauthCallback");
+    
   } else {
     // session 에서 accessToken을 가져옴.
+    var accessToken = req.session.accessToken;
 
     // 회원정보(프로필) API를 호출하여 사용자 정보를 가져온다.
     request.get("https://apis.daum.net/user/v1/show.json", {
-      json: true
+      json: true,
+      headers: {
+        "Authorization": "Bearer " + accessToken.access_token
+      }
       // Access Token을 Authorization Header로 넘긴다.
     }, function(err, response, userInfo) {
-        // 이 시점에 해당 access token과 엮어서 user의 정보를 DB에 저장해둔다.
-        // 사용자 정보를 렌더링
-    });  
+      console.log(userInfo);
+      // 이 시점에 해당 access token과 엮어서 user의 정보를 DB에 저장해둔다.
+      // 사용자 정보를 렌더링
+      res.render("user", {userInfo: userInfo.result});
+    });
   }
 });
 
